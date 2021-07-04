eff04109b660b734b5828aa7879887280fe762f9
voldie <voldiekami@gmail.com>
2021-07-04 18:34:21 +0200

    Code formated

--- frag-core/include/Prerequisites.h
+++ frag-core/include/Prerequisites.h
@@ -1,26 +1,26 @@
 /**
 	FragEngine, A Two layer Game Engine.
-    Copyright (C) 2018  Valdemar Lindberg
+	Copyright (C) 2018  Valdemar Lindberg
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 #ifndef _FRAG_CORE_PREREQUISITES_H_
 #define _FRAG_CORE_PREREQUISITES_H_ 1
-#include"Audio/Prerequisites.h"
-#include"Physic/Prerequisites.h"
-#include"Renderer/Prerequisites.h"
+#include "Audio/Prerequisites.h"
+#include "Physic/Prerequisites.h"
+#include "Renderer/Prerequisites.h"
 
 namespace fragcore {
 
@@ -52,8 +52,7 @@ namespace fragcore {
 	class IP;
 	class NetSocket;
 
-
-	//class SandBoxSubScene;
+	// class SandBoxSubScene;
 	class RenderPipeline;
 
 	/**
@@ -61,13 +60,11 @@ namespace fragcore {
 	 */
 	class TargetNode;
 
-
 	class Font;
 	class FontFactory;
 
 	class VideoFactory;
 	class VideoTexture;
-}
-
+} // namespace fragcore
 
 #endif
