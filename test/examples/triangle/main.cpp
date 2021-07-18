74ef7a936c73d39b6d545c966bda549452d254cd
voldie <voldiekami@gmail.com>
2021-07-18 13:42:22 +0200

    Resolved include path

--- test/examples/triangle/main.cpp
+++ test/examples/triangle/main.cpp
@@ -1,15 +1,14 @@
+#include <Audio/AudioClip.h>
+#include <Audio/AudioListener.h>
+#include <Audio/AudioSource.h>
+#include <Audio/decoder/AudioDecoder.h>
+#include <Audio/decoder/VorbisDecoder.h>
 #include <FragCore.h>
-#include <audio/AudioClip.h>
-#include <audio/AudioListener.h>
-#include <audio/AudioSource.h>
-#include <audio/decoder/AudioDecoder.h>
-#include <audio/decoder/VorbisDecoder.h>
 #include <ogg/ogg.h>
 #include <opus/opus.h>
 
 using namespace fragcore;
 
-int main(int argc, const char **argv)
-{
-	// 
+int main(int argc, const char **argv) {
+	//
 }
