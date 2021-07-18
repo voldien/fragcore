3b5aadd9232e80dd948d70d55dc19a350a733b0c
voldie <voldiekami@gmail.com>
2021-07-18 12:44:53 +0200

    Added init serial impl

---
+++ frag-core/include/Core/IO/SerialIO.h
@@ -0,0 +1,67 @@
+/**
+	FragEngine, A Two layer Game Engine.
+	Copyright (C) 2018  Valdemar Lindberg
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef _FRAG_CORE_SERIAL_FILE_IO_H_
+#define _FRAG_CORE_SERIAL_FILE_IO_H_ 1
+#include "../../Def.h"
+#include "../Ref.h"
+#include "IO.h"
+#include "ZipFileSystem.h"
+#include <libserialport.h>
+#include <optional>
+#include <vector>
+
+namespace fragcore {
+	/**
+	 * @brief
+	 *
+	 */
+	class SerialIO : public IO {
+	  public:
+		SerialIO(const std::string &path, Mode mode);
+
+		virtual void close(void) = 0;
+
+		virtual long int read(long int nbytes, void *pbuffer) = 0;
+
+		virtual long int write(long int nbytes, const void *pbuffer) = 0;
+
+		virtual bool eof(void) const = 0;
+
+		virtual long int length(void) = 0;
+
+		virtual void seek(long int nbytes, Seek seek) = 0;
+
+		virtual unsigned long getPos(void) = 0;
+
+		virtual bool isWriteable(void) const = 0;
+
+		virtual bool isReadable(void) const = 0;
+
+		virtual bool flush(void) = 0;
+
+		/**
+		 *
+		 */
+		static std::optional<std::vector<std::string>> getSerialPorts(void) noexcept;
+
+	  private:
+		struct sp_port *port;
+	};
+} // namespace fragcore
+#endif
---
+++ frag-core/src/core/io/SerialIO.cpp
@@ -0,0 +1,68 @@
+#include "Core/IO/SerialIO.h"
+#include <libserialport.h>
+#include <optional>
+
+using namespace fragcore;
+
+void SerialIO::close(void) {
+	struct sp_port *serialPort = (struct sp_port *)this->port;
+	sp_close(serialPort);
+	this->port = nullptr;
+}
+long int SerialIO::read(long int nbytes, void *pbuffer) {
+	struct sp_port *serialPort = (struct sp_port *)this->port;
+	nbytes = sp_nonblocking_read(serialPort, pbuffer, nbytes);
+	return nbytes;
+}
+long int SerialIO::write(long int nbytes, const void *pbuffer) {
+	struct sp_port *serialPort = (struct sp_port *)this->port;
+	nbytes = sp_nonblocking_write(serialPort, pbuffer, nbytes);
+	sp_drain(serialPort);
+	return nbytes;
+}
+bool SerialIO::eof(void) const {}
+long int SerialIO::length(void) {}
+void SerialIO::seek(long int nbytes, Seek seek) {}
+unsigned long SerialIO::getPos(void) {}
+bool SerialIO::isWriteable(void) const {}
+bool SerialIO::isReadable(void) const {}
+bool SerialIO::flush(void) {}
+
+SerialIO::SerialIO(const std::string &path, Mode mode) {
+	const int baudRate = 115200;
+
+	sp_return res = sp_get_port_by_name(path.c_str(), (struct sp_port **)&this->port);
+	if (res != SP_OK)
+		throw RuntimeException("Failed get this->port {} - {}", path, sp_last_error_message());
+	res = sp_open((struct sp_port *)this->port, SP_MODE_READ_WRITE);
+	if (res != SP_OK)
+		throw RuntimeException("Failed to open {} - {}", path, sp_last_error_message());
+	res = sp_set_baudrate((struct sp_port *)this->port, baudRate);
+	if (res != SP_OK)
+		throw RuntimeException("Failed to set baud Rate {} - {}", baudRate, sp_last_error_message());
+	res = sp_set_parity((struct sp_port *)this->port, SP_PARITY_NONE);
+	if (res != SP_OK)
+		throw RuntimeException("Failed to disable parity check {} - {}", path, sp_last_error_message());
+	res = sp_set_flowcontrol((struct sp_port *)this->port, SP_FLOWCONTROL_NONE);
+	if (res != SP_OK)
+		throw RuntimeException("Failed to disable flow control {} - {}", path, sp_last_error_message());
+	res = sp_set_xon_xoff((struct sp_port *)this->port, SP_XONXOFF_DISABLED);
+	if (res != SP_OK)
+		throw RuntimeException("Failed to disable xonoff {} - {}", path, sp_last_error_message());
+}
+std::optional<std::vector<std::string>> SerialIO::getSerialPorts(void) noexcept {
+	int i;
+	struct sp_port **ports;
+	std::vector<std::string> list;
+	sp_return error = sp_list_ports(&ports);
+	if (error == SP_OK) {
+
+		for (i {} ports[i] != nullptr; i++) {
+			list.push_back(sp_get_port_name(ports[i]));
+		}
+		sp_free_port_list(ports);
+		return {list};
+	} else {
+		return {};
+	}
+}
