be681a49dfc38712b54f572dbb4fd3e7ea3d0156
Valdemar Lindberg <voldiekami@gmail.com>
2021-11-30 06:50:17 +0100

    Relocated code

---
+++ test/assert/src/ThreadingTest.cpp
@@ -0,0 +1,14 @@
+#include <Core/Hash.h>
+#include <Core/IO/BufferIO.h>
+#include <Core/IO/FileIO.h>
+#include <Core/IO/FileSystem.h>
+#include <Core/IO/ZipFileIO.h>
+#include <Core/IO/ZipFileSystem.h>
+#include <Core/RefPtr.h>
+#include <FragCore.h>
+#include <Window/Display.h>
+#include <gtest/gtest.h>
+
+#include "Core/IO/GZFileIO.h"
+
+using namespace fragcore;
